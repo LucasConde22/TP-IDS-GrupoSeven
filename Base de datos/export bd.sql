47ae81af886cb04d9b528681dc2f75400741941c
Lucas Ariel Conde Cardo <conde.cardo.lucas@gmail.com>
2024-06-01 22:52:11 -0300

    Creacion de tablas tipos_habitaciones y habitaciones, agregado de info. Corresponde a las tareas de id 24, 57 y 58.

--- Base de datos/export bd.sql
+++ Base de datos/export bd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 02-06-2024 a las 01:42:14
+-- Tiempo de generación: 02-06-2024 a las 03:50:48
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -24,6 +24,60 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `habitaciones`
+--
+
+CREATE TABLE `habitaciones` (
+  `numero` int(11) NOT NULL,
+  `tipo` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `habitaciones`
+--
+
+INSERT INTO `habitaciones` (`numero`, `tipo`) VALUES
+(301, 'deluxe'),
+(302, 'deluxe'),
+(201, 'master'),
+(202, 'master'),
+(203, 'master'),
+(204, 'master'),
+(205, 'master'),
+(101, 'simple'),
+(102, 'simple'),
+(103, 'simple'),
+(104, 'simple'),
+(105, 'simple'),
+(106, 'simple'),
+(107, 'simple'),
+(108, 'simple');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipos_habitaciones`
+--
+
+CREATE TABLE `tipos_habitaciones` (
+  `tipo` varchar(20) NOT NULL,
+  `caracteristicas` text NOT NULL,
+  `capacidad` int(11) NOT NULL,
+  `precio` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `tipos_habitaciones`
+--
+
+INSERT INTO `tipos_habitaciones` (`tipo`, `caracteristicas`, `capacidad`, `precio`) VALUES
+('deluxe', 'A definir', 6, 80),
+('master', 'A definir', 4, 35),
+('simple', 'A definir', 2, 25);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuarios`
 --
 
@@ -36,8 +90,28 @@ CREATE TABLE `usuarios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `usuario`, `email`, `contra`) VALUES
+(1, 'Usuario De Prueba', 'test_usuario1', 'test_usuario1@ids.com', '12345678');
+
+--
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `habitaciones`
+--
+ALTER TABLE `habitaciones`
+  ADD PRIMARY KEY (`numero`),
+  ADD KEY `tipo_fk` (`tipo`);
+
+--
+-- Indices de la tabla `tipos_habitaciones`
+--
+ALTER TABLE `tipos_habitaciones`
+  ADD PRIMARY KEY (`tipo`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -53,7 +127,17 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `habitaciones`
+--
+ALTER TABLE `habitaciones`
+  ADD CONSTRAINT `habitaciones_ibfk_1` FOREIGN KEY (`tipo`) REFERENCES `tipos_habitaciones` (`tipo`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
