794b99e45346b5b02196340b7fc1b9a0c7b1ed3b
Lucas Ariel Conde Cardo <conde.cardo.lucas@gmail.com>
2024-06-02 20:02:50 -0300

    Implementación de función 'reservar' en API Corresponde a tarea de ID 62 (no terminada)

--- Base de datos/api.py
+++ Base de datos/api.py
@@ -1,9 +1,51 @@
 from flask import Flask, jsonify, request
 from sqlalchemy import create_engine, text
 from sqlalchemy.exc import SQLAlchemyError
+import datetime
+
+def calcular_noches(entrada, salida): # Falta implementar en función reservar
+    return (datetime.strptime(entrada, '%Y-%m-%d') - datetime.strptime(salida, '%Y-%m-%d')).days
 
 app = Flask(__name__)
 engine = create_engine("mysql+mysqlconnector://root@localhost/tp_ids")
+
+@app.route('/reservar', methods = ['POST'])
+def reservar():
+    conn = engine.connect()
+    reserva = request.get_json()
+    
+    query = text(f"""
+            SELECT h.numero FROM habitaciones h
+            WHERE h.tipo = '{reserva["tipo"]}'
+            AND h.numero NOT IN (
+                SELECT r.habitacion FROM reservas r
+                WHERE '{reserva["entrada"]}' < r.salida AND '{reserva["salida"]}' > r.entrada
+            )
+            LIMIT 1
+        """)
+    
+    try:
+        result = conn.execute(query)
+    except SQLAlchemyError as err:
+        conn.close()
+        return jsonify({'message': 'Se ha producido un error: ' + str(err.__cause__)}), 500
+    
+    habitacion = result.fetchone()
+    if habitacion is None:
+        return jsonify({'message': f"No hay disponibilidad de ese tipo de habitaciones en esa fecha"}), 404
+
+    query = text(f"""
+            INSERT INTO reservas (usuario, habitacion, entrada, salida, valor)
+            VALUES ({reserva['usuario']}, {habitacion[0]}, '{reserva['entrada']}', '{reserva['salida']}', 
+                (SELECT precio FROM tipos_habitaciones WHERE tipo = '{reserva['tipo']}'))""")
+    try:
+        conn.execute(query)
+        conn.commit()
+    except SQLAlchemyError as err:
+        conn.close()
+        return jsonify({'message': 'Se ha producido un error: ' + str(err.__cause__)}), 500
+    conn.close()
+    return jsonify({'message': 'La reserva fue realizada!'}), 201
 
 @app.route('/loguear_usuario', methods = ['POST'])
 def loguear_usuario():
