b173382ecf37eaad792f596891aa4f3e4e787b8c
maru-b <mablanco@fi.uba.ar>
2024-06-03 10:14:11 -0300

    Se agrega errorhandler 500 y se crea su respectivo 500.html

--- frontend/app.py
+++ frontend/app.py
@@ -38,5 +38,9 @@ def restaurante():
 def page_not_found(e):
     return render_template("404.html"), 404
 
+@app.errorhandler(500)
+def server_error(e):
+    return render_template("500.html"), 500
+
 if __name__ == '__main__':
     app.run("127.0.0.1",port=8000,debug=True)
