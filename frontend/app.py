2c11875eaaac9ab44a90d7060e5afbbae9d62d22
eMILIO <emichelli@fi.uba.ar>
2024-06-04 18:42:23 -0300

    creacion de template opiniones y disenio form de opiniones

--- frontend/app.py
+++ frontend/app.py
@@ -50,5 +50,9 @@ def page_not_found(e):
 def server_error(e):
     return render_template("500.html"), 500
 
+@app.route("/opiniones")
+def opinion():
+    return render_template("opinion.html")
+
 if __name__ == '__main__':
     app.run("127.0.0.1",port=8000,debug=True)
