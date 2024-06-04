539c6a3b2619978a52844f6fb0feb332e5cadfd8
Lucas Ariel Conde Cardo <conde.cardo.lucas@gmail.com>
2024-06-03 21:12:05 -0300

    Terminada la función 'reservar' en la API Corresponde a tarea Nº62

--- Base de datos/api.py
+++ Base de datos/api.py
@@ -1,10 +1,14 @@
 from flask import Flask, jsonify, request
 from sqlalchemy import create_engine, text
 from sqlalchemy.exc import SQLAlchemyError
-import datetime
+from datetime import datetime
 
-def calcular_noches(entrada, salida): # Falta implementar en función reservar
-    return (datetime.strptime(entrada, '%Y-%m-%d') - datetime.strptime(salida, '%Y-%m-%d')).days
+def calcular_noches(entrada, salida):
+    """
+    Dadas dos fechas, una de entrada y otra de salida, calcula cuantos
+    dias durará la estadía.
+    """ 
+    return (datetime.strptime(salida, '%Y-%m-%d') - datetime.strptime(entrada, '%Y-%m-%d')).days
 
 app = Flask(__name__)
 engine = create_engine("mysql+mysqlconnector://root@localhost/tp_ids")
@@ -32,12 +36,20 @@ def reservar():
     
     habitacion = result.fetchone()
     if habitacion is None:
-        return jsonify({'message': f"No hay disponibilidad de ese tipo de habitaciones en esa fecha"}), 404
+        return jsonify({'message': f"No hay habitaciones disponibles para esas fechas"}), 404
+
+    try:
+        result = conn.execute(text(f"SELECT precio FROM tipos_habitaciones WHERE tipo = '{reserva['tipo']}'"))
+    except SQLAlchemyError as err:
+        conn.close()
+        return jsonify({'message': 'Se ha producido un error: ' + str(err.__cause__)}), 500
+    precio = result.fetchone()
+    noches = calcular_noches(reserva['entrada'], reserva['salida'])
+    valor_reserva = precio[0] * noches
 
     query = text(f"""
             INSERT INTO reservas (usuario, habitacion, entrada, salida, valor)
-            VALUES ({reserva['usuario']}, {habitacion[0]}, '{reserva['entrada']}', '{reserva['salida']}', 
-                (SELECT precio FROM tipos_habitaciones WHERE tipo = '{reserva['tipo']}'))""")
+            VALUES ({reserva['usuario']}, {habitacion[0]}, '{reserva['entrada']}', '{reserva['salida']}', '{valor_reserva}')""")
     try:
         conn.execute(query)
         conn.commit()
