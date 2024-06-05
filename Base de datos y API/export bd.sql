f48238ddd81e9ff3e07c89669bec32db719f3016
Lucas Ariel Conde Cardo <conde.cardo.lucas@gmail.com>
2024-06-04 22:01:54 -0300

    Modificación de tabla 'tipos_habitaciones' y agregado de info.

--- Base de datos y API/export bd.sql
+++ Base de datos y API/export bd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-06-2024 a las 02:17:38
+-- Tiempo de generación: 05-06-2024 a las 03:01:10
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -65,19 +65,28 @@ CREATE TABLE `reservas` (
   `habitacion` int(11) NOT NULL,
   `entrada` date NOT NULL,
   `salida` date NOT NULL,
-  `valor` int(11) NOT NULL
+  `valor` int(11) NOT NULL,
+  `huespedes` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `reservas`
 --
 
-INSERT INTO `reservas` (`numero`, `usuario`, `habitacion`, `entrada`, `salida`, `valor`) VALUES
-(14, 1, 101, '2024-10-01', '2024-10-10', 25),
-(15, 1, 102, '2024-10-01', '2024-10-10', 25),
-(16, 1, 301, '2024-10-20', '2024-10-30', 80),
-(17, 1, 302, '2024-10-20', '2024-10-30', 80),
-(21, 1, 201, '2024-10-15', '2024-10-25', 350);
+INSERT INTO `reservas` (`numero`, `usuario`, `habitacion`, `entrada`, `salida`, `valor`, `huespedes`) VALUES
+(14, 1, 101, '2024-10-01', '2024-10-10', 25, 0),
+(15, 1, 102, '2024-10-01', '2024-10-10', 25, 0),
+(16, 1, 301, '2024-10-20', '2024-10-30', 80, 0),
+(17, 1, 302, '2024-10-20', '2024-10-30', 80, 0),
+(21, 1, 201, '2024-10-15', '2024-10-25', 350, 0),
+(22, 1, 202, '2024-10-15', '2024-10-25', 350, 0),
+(23, 1, 101, '2024-06-24', '2024-06-30', 150, 0),
+(24, 1, 201, '2024-07-10', '2024-07-17', 245, 0),
+(25, 1, 101, '2024-05-30', '2024-06-05', 150, 0),
+(26, 2, 103, '2024-09-13', '2024-10-16', 825, 0),
+(27, 2, 301, '2024-06-05', '2024-06-07', 160, 0),
+(28, 2, 201, '2024-06-12', '2024-07-04', 770, 2),
+(29, 2, 202, '2024-06-06', '2024-06-22', 560, 1);
 
 -- --------------------------------------------------------
 
@@ -89,17 +98,22 @@ CREATE TABLE `tipos_habitaciones` (
   `tipo` varchar(20) NOT NULL,
   `caracteristicas` text NOT NULL,
   `capacidad` int(11) NOT NULL,
-  `precio` int(11) NOT NULL
+  `precio` int(11) NOT NULL,
+  `foto1` text DEFAULT NULL,
+  `foto2` text DEFAULT NULL,
+  `foto3` text DEFAULT NULL,
+  `foto4` text DEFAULT NULL,
+  `video` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `tipos_habitaciones`
 --
 
-INSERT INTO `tipos_habitaciones` (`tipo`, `caracteristicas`, `capacidad`, `precio`) VALUES
-('deluxe', 'A definir', 6, 80),
-('master', 'A definir', 4, 35),
-('simple', 'A definir', 2, 25);
+INSERT INTO `tipos_habitaciones` (`tipo`, `caracteristicas`, `capacidad`, `precio`, `foto1`, `foto2`, `foto3`, `foto4`, `video`) VALUES
+('deluxe', 'Nuestras lujosas Habitaciones Deluxe ofrecen todo lo que necesita para unas vacaciones de ensueño. Con capacidad para hasta 6 personas, estas habitaciones incluyen todas las comodidades que puede necesitar, como baño privado, televisión por cable, Wi-Fi Gratis, teléfono, microondas y aire acondicionado. Además ofrecen impresionantes vistas al lago desde dos balcones privados. Disfrute de una cocina completa, jacuzzi privado y acceso exclusivo al cerro, donde podrá practicar deportes de nieve. Sumérjase en la experiencia definitiva de lujo y comodidad durante su estancia con nosotros. ', 6, 80, 'room-2.jpg', 'bathroom-2.jpg', 'room-6.jpg', 'room-2.jpg', 'video-hab.mp4'),
+('master', 'Nuestra exclusiva Habitación Master es perfecta para hasta cuatro personas, con la flexibilidad de contar con 2 camas matrimoniales o 4 individuales. Este espacio excepcional ofrece todas las comodidades que puede necesitar, como baño privado, televisión por cable, Wi-Fi Gratis, teléfono, microondas y aire acondicionado. Además, las habitaciones Master poseen un balcón privado que brinda una vista impresionante al lago. Disfrute de una estancia inolvidable con acceso completo a todas las ammenities del complejo, como el gimnasio y la piscina.', 4, 35, 'room-8.jpg', 'room-9.jpg', 'bathroom-3.jpg', 'images/room-3.jpg', 'video-hab.mp4'),
+('simple', 'Estas habitaciones, adecuadas para hasta dos personas, ofrecen una cama matrimonial o dos individuales. Son consideradas las habitaciones estándar del complejo, equipadas con baño privado, televisión por cable, teléfono, WI-FI, aire acondicionado y microondas. Además, los huéspedes tienen acceso a todas las comodidades del complejo, desayuno incluido, el gimnasio, la piscina, la sala de juegos, entre otros. Nexus Hotel cuenta con un total de ocho habitaciones de este tipo.', 2, 25, 'room-7.jpg', 'room-1.jpg', 'bathroom-1.jpg', 'images/room-5.jpg', 'video-hab.mp4');
 
 -- --------------------------------------------------------
 
@@ -120,7 +134,8 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nombre`, `usuario`, `email`, `contra`) VALUES
-(1, 'Usuario De Prueba', 'test_usuario1', 'test_usuario1@ids.com', '12345678');
+(1, 'Usuario De Prueba', 'test_usuario1', 'test_usuario1@ids.com', '12345678'),
+(2, 'Usuario De Prueba 2', 'test_usuario2', 'abcd@qwerty.com', '12345678');
 
 --
 -- Índices para tablas volcadas
@@ -161,13 +176,13 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `reservas`
 --
 ALTER TABLE `reservas`
-  MODIFY `numero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `numero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Restricciones para tablas volcadas
